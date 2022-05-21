8a48566cb3f54a2cf9be69159e86eb9648fcfdc4
chlucas324 <chlucas324@gmail.com>
2022-05-21 18:57:24 -0400

    add role table

--- db/schema.sql
+++ db/schema.sql
@@ -5,3 +5,14 @@ CREATE TABLE department (
     department_name VARCHAR(30) NOT NULL,
     PRIMARY KEY(id)
 );
+
+-- role table
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY(id),
+    FOREIGN KEY(department_id)
+);
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,14 @@ VALUES
 ("Finance"),
 ("Legal"),
 ("Engineering");
+
+INTSERT INTO role(title, salary, department_id)
+VALUES
+('Sales Lead', '100000', '1'),
+('Salesperson', '80000', '1'),
+('Lead Engineer', '150000', '4'),
+('Software Engineer', '120000', '4'),
+('Account Manager', '160000', '2'),
+('Accountant', '125000', '2'),
+('Legal Team Lead', '250000', '3'),
+('Lawyer', '190000', '3');
